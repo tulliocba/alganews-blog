4172e5a3e9fc9b92be5bbd0ced5f961e1e255955
Tulio Gabriel <tulliocba@gmail.com>
2021-10-08 09:00:24 -0300

    Adding Syntax Highlighter

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,10 +1,39 @@
-import { ReactNode } from "react";
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown, { Components } from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark  as theme } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import gfm from "remark-gfm";
 
 interface MarkdownProps {
   markdown: string;
 }
 
+const components: Components = {
+  code({ node, inline, className, children, ...props }) {
+    const match = /language-(\w+)/.exec(className || "");
+    return !inline && match ? (
+      <SyntaxHighlighter
+        style={theme}
+        language={match[1]}
+        PreTag="div"
+      >
+        {String(children).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    ) : (
+      <code className={className} {...props}>
+        {children}
+      </code>
+    );
+  },
+};
+
 export const Markdown = ({ markdown }: MarkdownProps) => {
-  return <ReactMarkdown className="MarkdownRenderer">{markdown}</ReactMarkdown>;
+  return (
+    <ReactMarkdown
+      className="MarkdownRenderer"
+      plugins={[gfm]}
+      components={components}
+    >
+      {markdown}
+    </ReactMarkdown>
+  );
 };
